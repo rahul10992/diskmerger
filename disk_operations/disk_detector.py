d62ae4a8b6aedfab81e8396005e6ec9da9d1d446
rahul10992 <10383453+rahul10992@users.noreply.github.com>
2024-12-26 14:32:55 -0800

    Adding the file hashing for testng

--- disk_operations/disk_detector.py
+++ disk_operations/disk_detector.py
@@ -2,6 +2,7 @@ import os
 import logger.logger as log
 
 local_drive_name = {"Macintosh HD"}
+recovery_drive_name = {"Recovery"}
 
 
 def list_drives():
@@ -10,15 +11,16 @@ def list_drives():
 
 def get_external_drive_paths() -> list:
     drives = list_drives()
-    log.info("Detected external drives:")
+    log.debug("Detected external drives:")
     drive_paths = []
 
     for drive in drives:
-        if drive in local_drive_name:
+        if drive in local_drive_name or drive in recovery_drive_name:
             continue
 
         log.info("Checking: ", drive)
         log.debug("Navigating drive {}".format(drive))
         drive_paths.append(f"/Volumes/{drive}")
 
+    log.info(f"Detected {len(drive_paths)} external drives")
     return drive_paths
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-
+import hashlib
 import logger.logger
 from logger import logger as log
 from logger.log_levels import LogLevel
@@ -37,11 +37,38 @@ def main():
         # Testing mode
         parse_directory(path, file_names, duplicate_file_count, skipped_file_count)
 
+    check_files(file_names)
+
     print(
         "Found {} files".format(len(file_names)),
         "Duplicate files: {}".format(duplicate_file_count),
         "Skipped Dirs: {}".format(skipped_file_count),
     )
+
+
+def check_files(file_names):
+    files = file_names.keys()
+
+    for file in files:
+        log.debug("Checking file: ", file)
+        same_hash_files = {}
+        for file_path, file_hash in file_names[file]:
+
+            if file_hash in same_hash_files:
+                same_hash_files[file_hash].append(file_path)
+            else:
+                same_hash_files[file_hash] = [file_path]
+
+        for file_hash, file_paths in same_hash_files.items():
+            if len(file_paths) > 1:
+                log.error(
+                    "[DUPLICATE] ",
+                    file,
+                    " hash: ",
+                    file_hash,
+                    " paths: ",
+                    file_paths,
+                )
 
 
 def parse_directory(path, file_names, duplicate_file_count, skipped_file_count):
@@ -50,23 +77,7 @@ def parse_directory(path, file_names, duplicate_file_count, skipped_file_count):
 
     files = os.listdir(path)
     for file in files:
-        if not os.path.isdir(os.path.join(path, file)):
-            extension = file.split(".")[-1]
-            log.debug(file, "extension:", extension)
-            if file in file_names:
-                log.error(
-                    "[DUPLICATE] ",
-                    file,
-                    " existing path: ",
-                    file_names[file],
-                    " the other path: ",
-                    os.path.join(path, file),
-                )
-                duplicate_file_count += 1
-            else:
-                log.debug("Adding file: ", file)
-                file_names[file] = os.path.join(path, file)
-        else:
+        if os.path.isdir(os.path.join(path, file)):
             log.debug("\nParsing Directory: ", os.path.join(path, file), "\n")
             if not file.startswith("."):
                 parse_directory(
@@ -78,6 +89,27 @@ def parse_directory(path, file_names, duplicate_file_count, skipped_file_count):
             else:
                 log.warning("Skipping {} because it seems to be hidden".format(file))
                 skipped_file_count += 1
+        else:
+            if file in files_to_ignore:
+                log.debug("Skipping file: ", file)
+                continue
+            extension = file.split(".")[-1]
+            log.debug(file, "extension:", extension)
+            if file in file_names:
+                # log.error(
+                #     "[DUPLICATE] ",
+                #     file,
+                #     " existing path: ",
+                #     file_names[file],
+                #     " the other path: ",
+                #     os.path.join(path, file),
+                # )
+                duplicate_file_count += 1
+            else:
+                file_names[file] = []
+            full_file_path = os.path.join(path, file)
+            file_hash = hashlib.sha256(open(full_file_path, "rb").read()).hexdigest()
+            file_names[file].append((full_file_path, file_hash))
 
     log.debug("\nFinished parsing ", os.path.join(path))
 
